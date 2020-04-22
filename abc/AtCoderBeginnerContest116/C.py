74d54cc4197cf26f525ee650c6afed56ade1990c
takapdayon <sid.sug.toysolder@gmail.com>
2020-04-22 22:08:43 +0900

    117

--- abc/AtCoderBeginnerContest116/C.py
+++ abc/AtCoderBeginnerContest116/C.py
@@ -1,6 +1,18 @@
 def Grand_Garden(n, hl):
 
     ans = 0
+    maxlen = max(hl)
+    for i in range(maxlen):
+        for w in range(n):
+            if hl[w] == 0:
+                continue
+            elif w+1 == n:
+                ans += 1
+                hl[w] -= 1
+                continue
+            elif hl[w+1] == 0:
+                ans += 1
+            hl[w] -= 1
 
     return ans
 
