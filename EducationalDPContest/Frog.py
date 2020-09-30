cd71a36de9122ca8b7ca628c4c9312c4f083ae28
takapdayon <business.takap@gmail.com>
2020-09-30 23:42:56 +0900

    DP 最小化

--- EducationalDPContest/Frog.py
+++ EducationalDPContest/Frog.py
@@ -1,18 +1,18 @@
 def chmin(dp, i, a):
     if a < dp[i]:
-        dp[i] == a
+        dp[i] = a
         return True
     return False
 
 def frog(n, hlist):
 
-    dp = [10**5]*(n+1)
+    dp = [10**5]*(n)
     dp[0] = 0
 
-    for i in range(1, n+1):
-        chmin(dp, i, hlist[i-1])
+    for i in range(1, n):
+        chmin(dp, i, dp[i-1] + abs(hlist[i] - hlist[i-1]))
         if i > 1:
-            chmin(dp, i, hlist[i-2])
+            chmin(dp, i, dp[i-2] + abs(hlist[i] - hlist[i-2]))
     return dp[-1]
 
 def main():
