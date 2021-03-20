6cfacb42dc8a1f00d74cbc286bf0ecde46e60996
takapdayon <business.takap@gmail.com>
2021-03-20 22:56:48 +0900

    196

---
+++ abc/AtCoderBeginnerContest196/B.py
@@ -0,0 +1,8 @@
+def b196(x):
+
+    ans = list(map(str, x.split('.')))
+
+    return ans[0]
+
+x = str(input())
+print(b196(x))
---
+++ abc/AtCoderBeginnerContest196/D.py
@@ -0,0 +1,16 @@
+def d196(h, w, a, b):
+    sikaku = [[False]*w]*h
+    ans = 0
+    if a = 0:
+        return 1
+
+    
+
+    for h_h in h:
+        for w_w in w:
+
+
+    return ans
+
+h, w, a, b = map(int, input().split())
+print(d196(h, w, a, b))
