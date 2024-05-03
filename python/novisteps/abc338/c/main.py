e456744dd9bb71938cf38f0b4f92bede48bb49fb
takapdayon <business.takap@gmail.com>
2024-05-03 11:10:49 +0900

    novistep精進

---
+++ python/novisteps/abc338/c/main.py
@@ -0,0 +1,55 @@
+import sys, re
+from math import ceil, floor, inf, sqrt, pi, gcd, lcm, factorial, atan, degrees
+from copy import deepcopy
+from collections import Counter, deque, defaultdict
+from heapq import heapify, heappop, heappush
+from itertools import accumulate, product, combinations, combinations_with_replacement, permutations
+from bisect import bisect, bisect_left, bisect_right
+from functools import reduce, cache
+from decimal import Decimal, getcontext
+from sortedcontainers import SortedSet, SortedList, SortedDict
+
+# input = sys.stdin.readline
+def i_input(): return int(input())
+def i_map(): return map(int, input().split())
+def i_list(): return list(i_map())
+def i_row(N): return [i_input() for _ in range(N)]
+def i_row_list(N): return [i_list() for _ in range(N)]
+def s_input(): return input()
+def s_map(): return input().split()
+def s_list(): return list(s_map())
+def s_row(N): return [s_input() for _ in range(N)]
+def s_row_str(N): return [s_list() for _ in range(N)]
+def s_row_list(N): return [list(s_input()) for _ in range(N)]
+
+sys.setrecursionlimit(10 ** 6)
+
+INF = float('inf')
+MOD = 10 ** 9 + 7
+num_list = []
+str_list = []
+
+def main():
+    N = i_input()
+    qlist = i_list()
+    alist = i_list()
+    blist = i_list()
+    result = 0
+    for i in range(max(qlist) + 1):
+        y = inf
+        for n in range(N):
+            if qlist[n] < alist[n] * i:
+                y = -inf
+                continue
+            if blist[n] > 0:
+                y = min(y, (qlist[n] - alist[n] * i) // blist[n])
+        result = max(result, i + y)
+
+    print(result)
+
+if __name__ == '__main__':
+    main()
+
+# テスト: oj t -c 'poetry run python main.py'
+# 提出: acc s main.py -- --guess-python-interpreter pypy
+# 再帰あるとき:  acc s main.py -- --guess-python-interpreter cpython --language 5055
