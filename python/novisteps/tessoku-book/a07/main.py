8fafd5f20d5d4982c3b5edc6c7df7e4fd0ae0691
takapdayon <business.takap@gmail.com>
2024-05-08 23:34:39 +0900

    novisteps精進なのだ

---
+++ python/novisteps/tessoku-book/a07/main.py
@@ -0,0 +1,53 @@
+import sys, re
+from math import ceil, floor, sqrt, pi, gcd, lcm, factorial, atan, degrees
+from copy import deepcopy
+from collections import Counter, deque, defaultdict
+from heapq import heapify, heappop, heappush
+from itertools import accumulate, product, combinations, combinations_with_replacement, permutations
+from bisect import bisect, bisect_left, bisect_right
+from functools import reduce, cache
+from decimal import Decimal, getcontext
+from sortedcontainers import SortedSet, SortedList, SortedDict
+
+# input = sys.stdin.readline
+def i_input(): return int(input())
+def i_map(): return map(int, input().split())
+def i_list(): return list(i_map())
+def i_row(N): return [i_input() for _ in range(N)]
+def i_row_list(N): return [i_list() for _ in range(N)]
+def s_input(): return input()
+def s_map(): return input().split()
+def s_list(): return list(s_map())
+def s_row(N): return [s_input() for _ in range(N)]
+def s_row_str(N): return [s_list() for _ in range(N)]
+def s_row_list(N): return [list(s_input()) for _ in range(N)]
+
+sys.setrecursionlimit(10 ** 6)
+
+INF = float('inf')
+MOD = 10 ** 9 + 7
+num_list = []
+str_list = []
+
+def main():
+    D = i_input()
+    N = i_input()
+    lrrows = i_row_list(N)
+    events = [0] * (D + 2)
+
+    for l, r in lrrows:
+        events[l] += 1
+        events[r + 1] -= 1
+
+    for d in range(1, D + 1):
+        events[d] += events[d - 1]
+
+    for event in events[1:D + 1]:
+        print(event)
+
+if __name__ == '__main__':
+    main()
+
+# テスト: oj t -c 'poetry run python main.py'
+# 提出: acc s main.py -- --guess-python-interpreter pypy
+# 再帰あるとき:  acc s main.py -- --guess-python-interpreter cpython --language 5055
