a01da8e5531fbcbe779ed729516cff0c76fdd23f
takapdayon <business.takap@gmail.com>
2024-05-13 01:08:56 +0900

    add: abc353

---
+++ python/abc/abc353/d/main.py
@@ -0,0 +1,51 @@
+import sys, re
+from math import ceil, floor, sqrt, pi, gcd, lcm, factorial, atan, degrees
+from copy import deepcopy
+from collections import Counter, deque, defaultdict
+from heapq import heapify, heappop, heappush
+from itertools import accumulate, product, combinations, combinations_with_replacement, permutations
+from bisect import bisect, bisect_left, bisect_right
+from functools import reduce, cache
+from decimal import Decimal, getcontext
+from sortedcontainers import SortedSet, SortedList, SortedDict
+
+# input = sys.stdin.readline
+def i_input(): return int(input())
+def i_map(): return map(int, input().split())
+def i_list(): return list(i_map())
+def i_row(N): return [i_input() for _ in range(N)]
+def i_row_list(N): return [i_list() for _ in range(N)]
+def s_input(): return input()
+def s_map(): return input().split()
+def s_list(): return list(s_map())
+def s_row(N): return [s_input() for _ in range(N)]
+def s_row_str(N): return [s_list() for _ in range(N)]
+def s_row_list(N): return [list(s_input()) for _ in range(N)]
+
+sys.setrecursionlimit(10 ** 6)
+
+INF = float('inf')
+MOD = 998244353
+num_list = []
+str_list = []
+
+def main():
+    N = i_input()
+    A = i_list()
+    result = 0
+    count = [0] * 11
+
+    for i, a in enumerate(reversed(A), 1):
+        result += (a * (N - i))
+        for keta in range(1, 11):
+            if count[keta]:
+                result += a * ((10 ** keta) * count[keta])
+        count[len(str(a))] += 1
+    print(result % MOD)
+
+if __name__ == '__main__':
+    main()
+
+# テスト: oj t -c 'poetry run python main.py'
+# 提出: acc s main.py -- --guess-python-interpreter pypy
+# 再帰あるとき:  acc s main.py -- --guess-python-interpreter cpython --language 5055
